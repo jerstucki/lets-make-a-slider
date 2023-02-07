10fd491681809964fab15f02d1570b1c34ff6d10
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-02-07 11:33:50 +0100

    render slider value

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Slider } from "./Slider";
 import "./index.css";
 
+const App = () => {
+  const [sliderValue, setSliderValue] = useState(0);
+
+  return (
+    <div className="container">
+      <div className="AppSliderValue">{sliderValue.toFixed(0)}</div>
+      <Slider onChange={(value) => setSliderValue(value)} />
+    </div>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="container">
-      <Slider />
-    </div>
+    <App />
   </React.StrictMode>
 );
