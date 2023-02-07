eeb58d82b21b39e16e11b0354638c0d30a2d7442
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-02-07 11:05:15 +0100

    draggable slider thumb

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,12 +1,21 @@
+import { motion } from "framer-motion";
 import "./Slider.css";
 
 export function Slider() {
+  const sliderWidth = 400;
+
   return (
-    <div className="Slider" style={{ width: 400 }}>
+    <div className="Slider" style={{ width: sliderWidth }}>
       <div className="SliderTrack">
         <div className="SliderTrackFill"></div>
       </div>
-      <div className="SliderThumb"></div>
+      <motion.div
+        className="SliderThumb"
+        drag
+        dragConstraints={{ left: 0, right: sliderWidth, top: 0, bottom: 0 }}
+        dragElastic={false}
+        dragMomentum={false}
+      ></motion.div>
     </div>
   );
 }
