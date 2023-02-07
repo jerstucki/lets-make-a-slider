8bc483366f444bac915412de00d932d06bf4aaf3
Jeremy Stucki <jeremy.stucki@gmail.com>
2023-02-07 11:11:42 +0100

    sync thumb and track fill using a motion value

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,16 +1,28 @@
-import { motion } from "framer-motion";
+import { motion, useMotionValue, useMotionValueEvent } from "framer-motion";
 import "./Slider.css";
 
 export function Slider() {
   const sliderWidth = 400;
 
+  const x = useMotionValue(0);
+
+  useMotionValueEvent(x, "change", (xValue) => {
+    console.log(xValue);
+  });
+
   return (
     <div className="Slider" style={{ width: sliderWidth }}>
       <div className="SliderTrack">
-        <div className="SliderTrackFill"></div>
+        <motion.div
+          className="SliderTrackFill"
+          style={{ width: x }}
+        ></motion.div>
       </div>
       <motion.div
         className="SliderThumb"
+        style={{
+          x,
+        }}
         drag
         dragConstraints={{ left: 0, right: sliderWidth, top: 0, bottom: 0 }}
         dragElastic={false}
